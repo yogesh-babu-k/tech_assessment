5e91317e3434dd3f59e0bf293e1060ac56769dce
Yogesh Babu Krishnakumar <63194234+yogesh-babu-k@users.noreply.github.com>
2021-12-26 22:41:31 +1100

    Update insert_sec2_1_city.sql

--- section_2/insert_sec2_1_city.sql
+++ section_2/insert_sec2_1_city.sql
@@ -1,22 +1,15 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2513
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+-----------------------------------------
+--
+-- Author: Yogesh Babu K
+--
+--	File Name: insert_sec2_1_city.sql
+--
+--	Created Date: 25 Dec 2021
+--	
+-----------------------------------------
 
-\f0\fs24 \cf0 #########################################\
-#\
-#	Author: Yogesh Babu K\
-#\
-#	File Name: insert_sec2_1_city.sql\
-#\
-#	Created Date: 25 Dec 2021\
-#	\
-#########################################\
-\
-INSERT INTO CITY (ID, NAME, COUNTRYCODE, DISTRICT, POPULATION) VALUES (1660, 'city1', 'c01', 'district1', 100000);\
-INSERT INTO CITY (ID, NAME, COUNTRYCODE, DISTRICT, POPULATION) VALUES (1661, 'city2', 'c01', 'district2', 200000);\
-INSERT INTO CITY (ID, NAME, COUNTRYCODE, DISTRICT, POPULATION) VALUES (1662, 'city3', 'c01', 'district3', 230000);\
-INSERT INTO CITY (ID, NAME, COUNTRYCODE, DISTRICT, POPULATION) VALUES (1663, 'city4', 'c01', 'district2', 400000);\
-INSERT INTO CITY (ID, NAME, COUNTRYCODE, DISTRICT, POPULATION) VALUES (1664, 'city5', 'c01', 'district1', 100000);}
+INSERT INTO CITY (ID, NAME, COUNTRYCODE, DISTRICT, POPULATION) VALUES (1660, 'city1', 'c01', 'district1', 100000);
+INSERT INTO CITY (ID, NAME, COUNTRYCODE, DISTRICT, POPULATION) VALUES (1661, 'city2', 'c01', 'district2', 200000);
+INSERT INTO CITY (ID, NAME, COUNTRYCODE, DISTRICT, POPULATION) VALUES (1662, 'city3', 'c01', 'district3', 230000);
+INSERT INTO CITY (ID, NAME, COUNTRYCODE, DISTRICT, POPULATION) VALUES (1663, 'city4', 'c01', 'district2', 400000);
+INSERT INTO CITY (ID, NAME, COUNTRYCODE, DISTRICT, POPULATION) VALUES (1664, 'city5', 'c01', 'district1', 100000);
