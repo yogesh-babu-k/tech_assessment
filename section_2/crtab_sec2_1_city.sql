4d98928a965a33416e2f73bb7ed6663e9b20bd52
Yogesh Babu Krishnakumar <63194234+yogesh-babu-k@users.noreply.github.com>
2021-12-26 22:38:02 +1100

    Update crtab_sec2_1_city.sql

--- section_2/crtab_sec2_1_city.sql
+++ section_2/crtab_sec2_1_city.sql
@@ -1,24 +1,17 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2513
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+#########################################
+#
+#	Author: Yogesh Babu K
+#
+#	File Name: crtab_sec2_1_city.sql
+#
+#	Created Date: 25 Dec 2021
+#	
+#########################################
 
-\f0\fs24 \cf0 #########################################\
-#\
-#	Author: Yogesh Babu K\
-#\
-#	File Name: crtab_sec2_1_city.sql\
-#\
-#	Created Date: 25 Dec 2021\
-#	\
-#########################################\
-\
-CREATE TABLE CITY (\
-  ID INTEGER, \
-  NAME VARCHAR(17), \
-  COUNTRYCODE VARCHAR(3),\
-  DISTRICT VARCHAR(20),\
-  POPULATION INTEGER\
-);}
+CREATE TABLE CITY (
+  ID INTEGER, 
+  NAME VARCHAR(17), 
+  COUNTRYCODE VARCHAR(3),
+  DISTRICT VARCHAR(20),
+  POPULATION INTEGER
+);
