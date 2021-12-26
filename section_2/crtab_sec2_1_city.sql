6f9020de42b820f9e6dcb03dce39ff04eefcb779
Yogesh Babu Krishnakumar <63194234+yogesh-babu-k@users.noreply.github.com>
2021-12-26 22:40:37 +1100

    Update crtab_sec2_1_city.sql

--- section_2/crtab_sec2_1_city.sql
+++ section_2/crtab_sec2_1_city.sql
@@ -1,12 +1,12 @@
-#########################################
-#
-#	Author: Yogesh Babu K
-#
-#	File Name: crtab_sec2_1_city.sql
-#
-#	Created Date: 25 Dec 2021
-#	
-#########################################
+-----------------------------------------
+--
+-- Author: Yogesh Babu K
+--
+--	File Name: crtab_sec2_1_city.sql
+--
+--	Created Date: 25 Dec 2021
+--	
+-----------------------------------------
 
 CREATE TABLE CITY (
   ID INTEGER, 
