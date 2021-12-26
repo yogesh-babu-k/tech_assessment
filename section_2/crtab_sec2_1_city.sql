1f38b3b46c69abf7d157dc88e3565325cc4948df
Yogesh Babu Krishnakumar <63194234+yogesh-babu-k@users.noreply.github.com>
2021-12-26 22:42:27 +1100

    Update crtab_sec2_1_city.sql

--- section_2/crtab_sec2_1_city.sql
+++ section_2/crtab_sec2_1_city.sql
@@ -1,8 +1,8 @@
 -----------------------------------------
 --
---    Author: Yogesh Babu K
+--  Author: Yogesh Babu K
 --
---	File Name: crtab_sec2_1_city.sql
+--  File Name: crtab_sec2_1_city.sql
 --
 --	Created Date: 25 Dec 2021
 --	
